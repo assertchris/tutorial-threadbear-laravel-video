2dc4f210ca8236cea64fe500ba014876137992f9
Inani El Houssain <inanielhoussain@gmail.com>
2017-09-27 17:18:40 +0100

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
     public function index()
     {
         return view("products.index", [
-            "products" => Product::where("user_id", auth()->user()->id)->get(),
+            "products" => auth()->user()->products(),
         ]);
     }
 
